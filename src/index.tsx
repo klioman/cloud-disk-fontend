66028d8a688e989d727ae9e633b59921cbff2f90
Yuriy Vasiliev <vasiliev.y.wezom@gmail.com>
2021-01-12 10:05:57 +0200

    feat(styles): updated app styles

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { history } from 'routes/history';
 import { persistor, store } from 'redux/store';
 import App from './App';
 
-import 'assets/styles/index.scss';
+import 'assets/styles/index.less';
 
 import reportWebVitals from './reportWebVitals';
 
