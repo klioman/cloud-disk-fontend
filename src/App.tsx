ec520b2df421e901a81f417d6f28581e8511d556
Yuriy Vasiliev <vasiliev.y.wezom@gmail.com>
2021-02-09 14:02:04 +0200

    feat(np progress): remode np progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import React, { FC, useState } from 'react';
+import React, { FC } from 'react';
 import { Switch, Route, BrowserRouter, Redirect } from 'react-router-dom';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import NpProgress from 'components/np-progress';
 import { isAuth, isGuest } from 'routes/authMiddlewares';
 
 import Home from 'pages/home';
@@ -9,40 +7,13 @@ import Auth from 'pages/auth';
 
 // ==========================================:
 const App: FC = () => {
-	const [isLoading, setIsLoading] = useState(false);
-
-	const changeRouter = () => {
-		setIsLoading(!isLoading);
-	};
-
 	return (
 		<BrowserRouter>
-			<Route
-				render={({ location }) => (
-					<>
-						<NpProgress isAnimating={isLoading} />
-						<TransitionGroup className="route-wrapper">
-							<CSSTransition
-								classNames="fade"
-								key={location.pathname.split('/')[1]}
-								onEnter={() => {
-									changeRouter();
-								}}
-								onEntered={() => {
-									changeRouter();
-								}}
-								timeout={0}
-							>
-								<Switch>
-									<Route path="/" exact component={isAuth(Home)} />
-									<Route path="/auth" exact component={isGuest(Auth)} />
-									<Redirect to="/" />
-								</Switch>
-							</CSSTransition>
-						</TransitionGroup>
-					</>
-				)}
-			/>
+			<Switch>
+				<Route path="/" exact component={isAuth(Home)} />
+				<Route path="/auth" exact component={isGuest(Auth)} />
+				<Redirect to="/" />
+			</Switch>
 		</BrowserRouter>
 	);
 };
