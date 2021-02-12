5ad04dce7718455254dd2f9c5ffd881199c14a59
Yuriy Vasiliev <vasiliev.y.wezom@gmail.com>
2021-02-12 17:39:26 +0200

    feat(components): button
    added button

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,4 +1,7 @@
 import React, { FC, Suspense, lazy } from 'react';
+import { FolderAddOutlined } from '@ant-design/icons';
+import { Button, Tooltip } from 'antd';
+
 import BasicLayout from 'layouts/base';
 import SuspenseLoader from 'components/suspense-loader';
 import AppBreadcrumbs from 'components/breadcrumbs';
@@ -9,7 +12,12 @@ const HomeContent = lazy(() => import('contents/home'));
 const Home: FC = () => {
 	return (
 		<BasicLayout>
-			<AppBreadcrumbs />
+			<div className="file-list-header">
+				<AppBreadcrumbs />
+				<Tooltip title="Добавить папку">
+					<Button type="primary" icon={<FolderAddOutlined />} />
+				</Tooltip>
+			</div>
 			<Suspense fallback={<SuspenseLoader />}>
 				<HomeContent />
 			</Suspense>
